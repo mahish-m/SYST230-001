0034a6bc13ef124238fab30a2b7ab32b3b2f9763
cbevacquatech <70604427+cbevacquatech@users.noreply.github.com>
2022-05-05 23:13:04 -0400

    small integration fixes

--- backend/removepassword.py
+++ backend/removepassword.py
@@ -1,11 +1,10 @@
-def removepassword(key):
+from PassManager import *
+def removepassword(key, passName):
     decrypt("Vault.csv",key)
     df=pd.read_csv(r"Vault.csv")
     table = df.values.tolist()
-    newCompany = input("Password Name: ")
     for x in table:
-        if newCompany in x:
+        if passName in x:
             table.remove(x)
-    
     pd.DataFrame(table).to_csv("Vault.csv",index=False)
     encrypt("Vault.csv", getFernetKey("Input Password to Save Changes"))#then encrypt it again
